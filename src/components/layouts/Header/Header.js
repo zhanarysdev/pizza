fcb564f8d9b34bfc0f8bcefa5fac805fb15bdb3b
Zhanarys <zhanarysdev@gmail.com>
2018-04-08 13:38:31 +0600

    before eject

---
+++ src/components/layouts/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="col-md-4">
+            
+            </div>
+            <div className="col-md-4">
+                <a href=""><img src="" alt=""/></a>
+            </div>
+            <div className="col-md-4">
+            
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/layouts/Header/MealMenu.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class MealMenu extends Component {
+    render(){
+        return(
+            <div>
+                <div className="container" >
+                    <ul>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MealMenu;
+
